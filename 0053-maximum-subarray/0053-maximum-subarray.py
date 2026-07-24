3f649fcc632e0c7b48af9969dd317c87030a9300
madhan023-mn <madhangosikonda23@gmail.com>
2026-07-24 19:03:45 +0530

    Time: 16 ms (99.75%), Space: 21.9 MB (19.63%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        max_sum=float('-inf')
+        curr_sum=0
+        
+        for i in nums:
+            curr_sum+=i
+            
+            if curr_sum>max_sum:
+                max_sum=curr_sum 
+
+            if curr_sum<0:
+                curr_sum=0
+        return max_sum 
+
+        
+        
+
+        
+        
