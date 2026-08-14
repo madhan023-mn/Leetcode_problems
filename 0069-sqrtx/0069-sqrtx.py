d531dc2a282ef8404bfb1a574658806d13ab3ed4
Gosikonda Madhan <madhangosikonda23@gmail.com>
2026-08-14 10:40:13 +0530

    Time: 11 ms (17.43%), Space: 12.3 MB (90.18%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x<2:
+            return x
+        l,r=1,x//2
+        while l<=r:
+            mid=(l+r)//2 
+            if mid*mid <=x:
+                l=mid+1 
+            else:
+                r=mid-1 
+        return r
+        
+        
